13dec07c6fbdcecd6057f4bf488e7ea80ffbdc1b
Starfish <None.of.your@busine.ss>
2018-04-22 11:43:22 +1000

    more scripts

---
+++ run_with_computation.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -p 17876:17876 -p 17874:17874 -p 16876:16876 -p 16874:16874  --mount type=bind,source="$(pwd)"/xel_data_dir,target=/elastic-core-maven/nxt_test_db --mount type=bind,source="$(pwd)"/conf,target=/elastic-core-maven/conf -i -t xel
